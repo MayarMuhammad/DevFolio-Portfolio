555cc2454cd142fee3679a858406996f8f2f7fad
Mayar Muhammed <mayarmuhammad465@gmail.com>
2023-01-30 21:30:34 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,73 @@
+(function () {
+  "use strict";
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  const typed = select(".typed");
+  if (typed) {
+    let typed_strings = typed.getAttribute("data-typed-items");
+    typed_strings = typed_strings.split(",");
+    new Typed(".typed", {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+    });
+  }
+
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  function scrollValue() {
+    var navbar = document.getElementById("navbar");
+    var scroll = window.scrollY;
+    if (scroll < 50) {
+      navbar.classList.remove("bgColor");
+    } else {
+      navbar.classList.add("bgColor");
+    }
+  }
+  window.addEventListener("scroll", scrollValue);
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+
+  $(window).on('hashchange', function(e){
+    history.replaceState ("", document.title, e.originalEvent.oldURL);
+});
+
+
+  new PureCounter();
+})();
